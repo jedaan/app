2a216c26ba3e42c4bab842705fc65638710f2fa9
jedaan <jed3an@gmail.com>
2019-01-23 16:38:18 +0200

    fixes

--- src/client/client.js
+++ src/client/client.js
@@ -1,10 +1,14 @@
 // startup point of client side application
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import {BrowserRouter} from 'react-router-dom';
+import Routes from './Routes';
 
 
-ReactDOM.hydrate(<Home/>, document.querySelector('#root'));// its not replaced the dic content that
+ReactDOM.hydrate(
+    <BrowserRouter>
+        <Routes/>
+    </BrowserRouter>, document.querySelector('#root'));// its not replaced the dic content that
 //rendered by the server its add the necessary code .
 
 console.log('hi client');
--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
     // inform webpack that we're building a bundle
@@ -14,7 +15,9 @@ const config = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    }
+    },
+
+    externals:[webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
